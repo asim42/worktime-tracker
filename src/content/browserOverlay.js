597f9f20e75c44eb2d5adbe5641f745d6df292d8
asim <asimawan.42@gmail.com>
2015-08-02 02:41:34 +0500

    Added Switch on/off notifications

--- src/content/browserOverlay.js
+++ src/content/browserOverlay.js
@@ -12,7 +12,7 @@ WorkTimeTrackerChrome.BrowserOverlay = {
   /**
    * Says 'Hello' to the user.
    */
-  sayHello : function(aEvent) {
+  sayHello : function() {
     var stringBundle = document.getElementById("worktimetracker-string-bundle");
     var message = stringBundle.getString("worktimetracker.greeting.label");
 
@@ -22,5 +22,28 @@ WorkTimeTrackerChrome.BrowserOverlay = {
   showSettings : function(aEvent) {
       window.open("chrome://worktimetracker/content/settingsDialogue.xul",
           "worktimetracker-settings-dialogue", "chrome,centerscreen");
+  },
+  switchOn : function() {
+      var stringBundle = document.getElementById("worktimetracker-string-bundle");
+      var title = stringBundle.getString("worktimetracker.switch.notification.title");
+      var message = stringBundle.getString("worktimetracker.switch.on.notification.message");
+      notify(title, message);
+  },
+  switchOff : function() {
+    var stringBundle = document.getElementById("worktimetracker-string-bundle");
+    var title = stringBundle.getString("worktimetracker.switch.notification.title");
+    var message = stringBundle.getString("worktimetracker.switch.off.notification.message");
+    notify(title, message);
   }
 };
+
+function notify(title, message) {
+    try {
+        var img = "chrome://worktimetracker/skin/icon.png";
+        Components.classes['@mozilla.org/alerts-service;1']
+            .getService(Components.interfaces.nsIAlertsService)
+            .showAlertNotification(img, title, message, true, '', null);
+    }catch (e) {
+
+    }
+}
